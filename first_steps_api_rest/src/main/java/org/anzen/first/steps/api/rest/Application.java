e4dfaae1b72b08dc4e8be87f34c6984bcfe29cc5
Marco Acevedo <marcoacevedo@anzen.com.mx>
2018-02-14 12:45:24 -0600

    First commit into first steps

---
+++ first_steps_api_rest/src/main/java/org/anzen/first/steps/api/rest/Application.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Anzen Soluciones S.A. de C.V.
+ * Mexico D.F.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS  CONFIDENTIAL INFORMATION PROPIETARY OF ANZEN SOLUCIONES.
+ * THIS INFORMATION SHOULD NOT BE DISCLOSED AND MAY ONLY BE USED IN ACCORDANCE THE TERMS DETERMINED BY THE COMPANY ITSELF.
+ */
+package org.anzen.first.steps.api.rest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p></p>
+ * 
+ * @author Marco Acevedo {@literal <marcoacevedo@anzen.com.mx>}
+ * @version first-steps-api-rest
+ * @since first-steps-api-rest
+ * @category
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * <p></p>
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
